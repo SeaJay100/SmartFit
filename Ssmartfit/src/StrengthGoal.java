9f14de48889008c3c9b593bf1ae1ac4f0e6aaaaf
seajay <gomezcarl764@gmail.com>
2026-05-10 02:37:28 +0800

    COMPLETE FINAL SMART FITNESS SYSTEM

---
+++ Ssmartfit/src/StrengthGoal.java
@@ -0,0 +1,11 @@
+public class StrengthGoal extends GoalProfile {
+
+    public StrengthGoal() {
+        super(
+            "Strength",
+            "3-6",
+            false,
+            "Use heavier loads with lower repetitions to build strength."
+        );
+    }
+}
---
+++ Ssmartfit/src/WeightLossGoal.java
@@ -0,0 +1,11 @@
+public class WeightLossGoal extends GoalProfile {
+
+    public WeightLossGoal() {
+        super(
+            "Weight Loss",
+            "12-15",
+            true,
+            "Include cardio and use higher repetitions to improve calorie burn and endurance."
+        );
+    }
+}
